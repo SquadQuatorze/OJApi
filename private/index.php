3528fc1dddee8f17c82c7a1f3967e2707aa31af6
alvesgeraldo <jgalvesb@gmail.com>
2022-11-08 14:56:40 -0300

    Métodos POST e GET atualizados, inclusao verificarçao CORs

--- private/index.php
+++ private/index.php
@@ -1,80 +1,99 @@
 <?php
 
-  use \Psr\Http\Message\ServerRequestInterface as Request;
-  use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Tuupola\Middleware\CorsMiddleware as CorsMiddleware;
 
-  require '../OJ/vendor/autoload.php';
+require '../OJ/vendor/autoload.php';
 
-  // configurações do projeto
-  $config['displayErrorDetails'] = true;
+// configurações do projeto
+$config['displayErrorDetails'] = true;
 
-  // Instância do objeto API
-  $app = new \Slim\App(['settings' => $config]);
+// Instância do objeto API
+$app = new \Slim\App(['settings' => $config]);
 
-  // injeção de dependências necessárias para o projeto
-  $container = $app->getContainer();
+// injeção de dependências necessárias para o projeto
+$container = $app->getContainer();
 
-  $container['conexao'] = function(){
-    require '../private/Conexao.php';
-    return new Conexao();
-  };
+$container['conexao'] = function () {
+  require '../private/Conexao.php';
+  return new Conexao();
+};
 
-  $container['usuario'] = function(){
-    require '../private/usuarios/usuarios.model.php';
-    return new Usuario();
-  };
+$container['usuario'] = function () {
+  require '../private/usuarios/usuarios.model.php';
+  return new Usuario();
+};
 
-  $container['usuarioService'] = function($c){
-    require '../private/usuarios/usuarios.service.php';
-    return new UsuarioService($c['conexao'], $c['usuario']);
-  };
+$container['usuarioService'] = function ($c) {
+  require '../private/usuarios/usuarios.service.php';
+  return new UsuarioService($c['conexao'], $c['usuario']);
+};
 
-  $app->post('/usuarios', function(Request $request, Response $response){
-    
-    $conexao = $this->get('conexao');
-    $usuario = $this->get('usuario');
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+  return $response;
+});
 
-    $post = $request->getParsedBody();
-    $usuario->__set('email_usuario', $post['email_usuario']);
-    $usuario->__set('senha_usuario', $post['senha_usuario']);
-    $usuario->__set('nome_usuario', $post['nome_usuario']);
-    
-    $usuarioService = $this->get('usuarioService');
-    
+$app->add(function ($req, $res, $next) {
+  $response = $next($req, $res);
+  return $response
+    ->withHeader('Access-Control-Allow-Origin', '*')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+$app->post('/usuarios', function (Request $request, Response $response) {
+
+  $conexao = $this->get('conexao');
+  $usuario = $this->get('usuario');
+
+  $post = $request->getParsedBody();
+  $usuario->__set('email_usuario', $post['email']);
+  $usuario->__set('senha_usuario', $post['password']);
+  $usuario->__set('nome_usuario', $post['nome']);
+
+  $usuarioService = $this->get('usuarioService');
+
+  $usuarios = $usuarioService->buscarPorEmail();
+
+  if (count($usuarios) == 0) {
+    $response->getBody()->write($usuarioService->cadastrar());
+  } else {
+    $response->getBody()->write('Email já cadastrado');
+  }
+
+  return $response;
+});
+
+$app->get('/usuarios', function (Request $request, Response $response) {
+
+  $conexao = $this->get('conexao');
+  $usuario = $this->get('usuario');
+
+  $post = $request->getParsedBody();
+  $email = $post['email'];
+
+  $usuarioService = $this->get('usuarioService');
+
+  if ($email != '') {
+    $usuario->__set('email_usuario', $email);
     $usuarios = $usuarioService->buscarPorEmail();
+  } else {
+    $usuarios = $usuarioService->buscarUsuarios();
+  }
 
-    if(count($usuarios) == 0){
-      $response->getBody()->write($usuarioService->cadastrar());
-    } else {
-      $response->getBody()->write('Email já cadastrado');
-    }
+  $response->getBody()->write(json_encode($usuarios));
+  return $response->withHeader('Access-Control-Allow-Origin', '*')
+    ->withAddedHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE')
+    ->withAddedHeader('Access-Control-Allow-Headers', '*')
+    ->withAddedHeader('Access-Control-Max-Age', '86400')
+    ->withAddedHeader('Content-Type', 'application/json');
+});
 
-    return $response;
-    
-  });
-
-  $app->get('/usuarios', function(Request $request, Response $response){
-
-    $conexao = $this->get('conexao');
-    $usuario = $this->get('usuario');
-
-    $post = $request->getParsedBody();
-    $email = $post['email'];
-
-    $usuarioService = $this->get('usuarioService');
-    
-    if($email != ''){
-      $usuario->__set('email_usuario', $email);
-      $usuarios = $usuarioService->buscarPorEmail();
-    } else {
-      $usuarios = $usuarioService->buscarUsuarios();
-    }
-
-    return $response->withJson($usuarios);
-    
-  });
-
-  $app->run();
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($req, $res) {
+  $handler = $this->notFoundHandler;
+  return $handler($req, $res);
+});
 
 
-?>
+$app->run();
